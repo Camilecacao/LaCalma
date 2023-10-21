39d73b9be1d976ffebd1507259d76dbc718b971d
Anti2023 <mariaantonietarivano@gmail.com>
2023-10-20 21:40:55 -0300

    agregué firebase

--- src/Router/MainRouter.jsx
+++ src/Router/MainRouter.jsx
@@ -1,17 +1,16 @@
 import { Route, Routes } from "react-router-dom"
 import { Home } from "../pages/Home"
-import {About} from "../pages/About"
-import {Menu} from "../pages/Menu"
-import {Reservation} from "../pages/Reservation"
+import { About } from "../pages/About"
+import { Menu } from "../pages/Menu"
+import { Reservation } from "../pages/Reservation"
 export const MainRouter = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/About" element={<About/>}/>
-        <Route path="/Menu" element={<Menu/>}/>
-        <Route path="/Reservation" element={<Reservation/>}/>
-        <Route path="/*" element={<h1>Error 404</h1>}/>
-
+      <Route path="/" element={<Home />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Menu" element={<Menu />} />
+      <Route path="/Reservation" element={<Reservation />} />
+      <Route path="/*" element={<h1>Error 404</h1>} />
     </Routes>
 
   )
---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZhZUe0kVjNFfaQZJxS4rR1vjMH_3Dxrw",
+  authDomain: "proyecto-cuatro-rest.firebaseapp.com",
+  projectId: "proyecto-cuatro-rest",
+  storageBucket: "proyecto-cuatro-rest.appspot.com",
+  messagingSenderId: "94412115195",
+  appId: "1:94412115195:web:623d4a506c26f9349ff45e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
